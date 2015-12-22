d89d846b222b0f792ed13d6ded5a56fd06f056cc
“student511” <abreu.wilbert@gmail.com>
2015-12-21 21:54:12 -0500

    "Refactored code to fit pep8 standards"

--- blog/manage.py
+++ blog/manage.py
@@ -1,39 +1,27 @@
-import os
 from flask.ext.script import Manager
-
 from blog import app
+from blog.database import session, Entry, User, Base
+from getpass import getpass
+from werkzeug.security import generate_password_hash
+from flask.ext.migrate import Migrate, MigrateCommand
 
-#add a task to your manager which generates a series of entries in the database
-from blog.database import session, Entry
-
-#import manager object and create an instance of it
 manager = Manager(app)
 
-#add a command to the manager
+
 @manager.command
 def run():
     app.run(host="0.0.0.0", port=8080)
+
 
 @manager.command
 def seed():
     content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit"""
 
     for i in range(25):
-        entry = Entry(
-             title = "Test Entry #{}".format(i),
-             content = content
-        )
+        entry = Entry(title = "Test Entry #{}".format(i), content = content)
         session.add(entry)
     session.commit()
 
-
-
-#add test user
-from getpass import getpass
-
-from werkzeug.security import generate_password_hash
-
-from blog.database import User
 
 @manager.command
 def adduser():
@@ -43,30 +31,20 @@ def adduser():
         print("User with that email address already exists")
         return
 
-    password = ""
+    password, password_2 = ""
     while len(password) < 8 or password != password_2:
         password = getpass("Password: ")
         password_2 = getpass("Re-enter password: ")
-        #generate_password_hash hashes the password
     user = User(name=name, email=email,
                 password=generate_password_hash(password))
     session.add(user)
     session.commit()
 
 
-#add migration command
-from flask.ext.migrate import Migrate, MigrateCommand
-from blog.database import Base
-
-#holds the metadata object
-# Alembic(migration tool) uses the metadata to work out the changes to the database schema
 class DB(object):
     def __init__(self, metadata):
         self.metadata = metadata
-#create instance of Flask's migrate class
-#pass in app and instance of Db class
 migrate = Migrate(app, DB(Base.metadata))
-#add migrate commands to the manager
 manager.add_command('db', MigrateCommand)
 
 
