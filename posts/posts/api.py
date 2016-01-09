4d1b733163dc60409fe11e8ca0dc9f79d0d99aab
“student511” <abreu.wilbert@gmail.com>
2016-01-09 13:11:56 -0500

    Using Query Strings in an API

--- posts/posts/api.py
+++ posts/posts/api.py
@@ -1,22 +1,30 @@
 import json
 from flask import request, Response, url_for
 from jsonschema import validate, ValidationError
-from . import models, decorators
+from . import models
+from . import decorators
 from posts import app
 from .database import session
-
-
-@app.route("/api/posts", methods=["GET"])
-def posts_get():
-    """ Get a list of posts """
-    pass
+from sqlalchemy import or_
 
 
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
     """ Get a list of posts """
-    posts = session.query(models.Post).order_by(models.Post.id)
+    title_like = request.args.get("title_like")
+    body_like = request.args.get("body_like")
+
+    posts = session.query(models.Post)
+    if title_like:
+        posts = posts.filter(models.Post.title.contains(title_like))
+    if body_like:
+        posts = posts.filter(models.Post.body.contains(body_like))
+    if title_like and body_like:
+        posts = posts.filter(or_(models.Post.title.contains(title_like),
+                                 models.Post.body.contains(body_like)))
+
+    posts = posts.order_by(models.Post.id)
     data = json.dumps([post.as_dictionary() for post in posts])
     return Response(data, 200, mimetype="application/json")
 
--- posts/tests/api_tests.py
+++ posts/tests/api_tests.py
@@ -71,7 +71,7 @@ class TestAPI(unittest.TestCase):
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data["message"], "Could not find post with id 1")
 
-     def test_unsupported_accept_header(self):
+    def test_unsupported_accept_header(self):
         response = self.client.get("/api/posts",
             headers=[("Accept", "application/xml")]
         )
@@ -82,6 +82,34 @@ class TestAPI(unittest.TestCase):
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data["message"],
                          "Request must accept application/json data")
+
+    def test_get_posts_with_title(self):
+        """ Filtering posts by title """
+        postA = models.Post(title="Post with bells", body="Just a test")
+        postB = models.Post(title="Post with whistles", body="Still a test")
+        postC = models.Post(title="Post with bells and whistles",
+                            body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=whistles",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with bells and whistles")
+        self.assertEqual(post["body"], "Another test")
 
 if __name__ == "__main__":
     unittest.main()
