97e00209d855988ecd53e17675a45c0e047ea86c
“student511” <abreu.wilbert@gmail.com>
2016-01-31 18:42:37 -0500

    Mas

--- python-flask-app-template/blog/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:wilbertabreu@locahost:5432/blogful"
-    DEBUG = True
--- tuneful/tuneful/api.py
+++ tuneful/tuneful/api.py
@@ -115,3 +115,5 @@ def song_delete(id):
 
 
 
+
+
